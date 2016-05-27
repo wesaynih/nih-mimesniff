1cbd76bea76f92b1f0948fa6b24475d1ecbfa983
Yolijn van der Kolk <mail@yolijn.com>
2016-05-27 23:28:33 +0200

    Match with multiple patterns, regexp values and fallback types

--- sniff-byte-pattern.js
+++ sniff-byte-pattern.js
@@ -5,27 +5,27 @@ var sniff = (function ()
     var bytePatterns = {
         'image/x-icon': {
             'byteNr':   [0, 1, 2, 3],
-            'patterns': [[0, 0x00, 0x01, 0x00], [0x00, 0x00, 0x02, 0x00]]
+            'patterns': [[0x00, 0x00, 0x01, 0x00], [0x00, 0x00, 0x02, 0x00]]
         },
         'image/bmp': {
             'byteNr':   [0, 1],
             'patterns': [0x42,  0x4D],
-            'regexp':   /^BM/
+            'regexp':   [/^BM/]
         },
         'image/gif': {
             'byteNr':   [0, 1, 2, 3, 4, 5],
             'patterns': [[0x47, 0x49, 0x46, 0x38, 0x37, 0x61], [0x47, 0x49, 0x46, 0x38, 0x39, 0x61]],
-            'regexp':   /^(GIF87a|GIF89a)/
+            'regexp':   [/^(GIF87a|GIF89a)/]
         },
         'image/webp': {
             'byteNr':   [0, 1, 2, 3, 8, 9, 10, 11, 12, 13],
             'patterns': [0x52, 0x49, 0x46, 0x46, 0x57, 0x45, 0x42, 0x50, 0x56, 0x50],
-            'regexp':   /^RIFF....WEBPVP/
+            'regexp':   [/^RIFF....WEBPVP/]
         },
         'image/png': {
             'byteNr':   [0, 1, 2, 3, 4, 5, 6, 7],
             'patterns': [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A],
-            'regexp':   /^\u0089PNG\u000D\u000A\u001A\u000A/
+            'regexp':   [/^\u0089PNG\u000D\u000A\u001A\u000A/]
         },
         'image/jpeg': {
             'byteNr':   [0, 1, 2],
@@ -34,42 +34,42 @@ var sniff = (function ()
         'audio/basic': {
             'byteNr':   [0, 1, 2, 3],
             'patterns': [0x2E, 0x73, 0x6E, 0x64],
-            'regexp':   /^.snd/
+            'regexp':   [/^.snd/]
         },
         'audio/aiff': {
             'byteNr':   [0, 1, 2, 3, 8, 9, 10, 11],
-            'patterns': [0x46, 0x4F, 0x52, 0x4D, 0x00, 0x00, 0x00, 0x00, 0x41, 0x49, 0x46, 0x46],
-            'regexp':   /^FORM....AIFF/
+            'patterns': [0x46, 0x4F, 0x52, 0x4D, 0x41, 0x49, 0x46, 0x46],
+            'regexp':   [/^FORM....AIFF/]
         },
         'audio/mpeg': {
             'byteNr':   [0, 1, 2],
             'patterns': [0x49, 0x44, 0x33],
-            'regexp':   /^ID3/
+            'regexp':   [/^ID3/]
         },
         'application/ogg': {
             'byteNr':   [0, 1, 2, 3, 4],
             'patterns': [0x4F, 0x67, 0x67, 0x53, 0x00],
-            'regexp':   /^OggS\0/
+            'regexp':   [/^OggS\0/]
         },
         'audio/midi': {
             'byteNr':   [0, 1, 2, 3, 4, 5, 6, 7],
             'patterns': [0x4D, 0x54, 0x68, 0x64, 0x00, 0x00, 0x00, 0x06],
-            'regexp':   /^MThd\0\0\0\u0006/
+            'regexp':   [/^MThd\0\0\0\u0006/]
         },
         'video/avi': {
             'byteNr':   [0, 1, 2, 3, 8, 9, 10, 11],
             'patterns': [0x52, 0x49, 0x46, 0x46, 0x41, 0x56, 0x49, 0x20],
-            'regexp':   /^RIFF....AVI/
+            'regexp':   [/^RIFF....AVI/]
         },
         'audio/wave': {
             'byteNr':   [0, 1, 2, 3, 8, 9, 10, 11],
             'patterns': [0x52, 0x49, 0x46, 0x46, 0x57, 0x41, 0x56, 0x45],
-            'regexp':   /^RIFF....WAVE/
+            'regexp':   [/^RIFF....WAVE/]
         },
         'application/vnd.ms-fontobject': {
             'byteNr':   [35, 36],
             'patterns': [0x4C, 0x50],
-            'regexp':   /^.{34}LP/
+            'regexp':   [/^.{34}LP/]
         },
         'application/x-unknown-content-type; name="TrueType"': {
             'byteNr':   [0, 1, 2, 3],
@@ -78,17 +78,17 @@ var sniff = (function ()
         'application/x-unknown-content-type; name="OpenType"': {
             'byteNr':   [0, 1, 2, 3],
             'patterns': [0x4F, 0x54, 0x54, 0x4F],
-            'regexp':   /^OTTO/
+            'regexp':   [/^OTTO/]
         },
         'application/x-unknown-content-type; name="TrueType Collection"': {
             'byteNr':   [0, 1, 2, 3],
             'patterns': [0x74, 0x74, 0x63, 0x66],
-            'regexp':   /^ttcf/
+            'regexp':   [/^ttcf/]
         },
         'application/font-woff': {
             'byteNr':   [0, 1, 2, 3],
             'patterns': [0x77, 0x4F, 0x46, 0x46],
-            'regexp':   /^wOFF/
+            'regexp':   [/^wOFF/]
         },
         'application/x-gzip': {
             'byteNr':   [0, 1, 2],
@@ -97,12 +97,12 @@ var sniff = (function ()
         'application/zip': {
             'byteNr':   [0, 1, 2, 3],
             'patterns': [0x50, 0x4B, 0x03, 0x04],
-            'regexp':   /^PK\u0003\u0004/
+            'regexp':   [/^PK\u0003\u0004/]
         },
         'application/x-rar-compressed': {
             'byteNr':   [0, 1, 2, 3, 4, 5, 6],
             'patterns': [0x52, 0x61, 0x72, 0x20, 0x1A, 0x07, 0x00],
-            'regexp':   /^Rar \u001A\u0007\0/
+            'regexp':   [/^Rar \u001A\u0007\0/]
         },
         'text/html': {
             'regexp':
@@ -130,17 +130,17 @@ var sniff = (function ()
         'text/xml': {
             'byteNr':   [0, 1, 2, 3, 4],
             'patterns': [0x3C, 0x3F, 0x78, 0x6D, 0x6C],
-            'regexp':   /^<[?]xml]/
+            'regexp':   [/^<[?]xml]/]
         },
         'application/pdf': {
             'byteNr':   [0, 1, 2, 3, 4],
             'patterns': [0x25, 0x50, 0x44, 0x46, 0x2D],
-            'regexp':   /^%PDF-/
+            'regexp':   [/^%PDF-/]
         },
         'application/postscript': {
             'byteNr':   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             'patterns': [0x25, 0x21, 0x50, 0x53, 0x2D, 0x41, 0x64, 0x6F, 0x62, 0x65, 0x2D],
-            'regexp':   /^%!PS-Adobe-/
+            'regexp':   [/^%!PS-Adobe-/]
         },
         'text/plain': {
             'byteNr':   [0, 1],
@@ -158,9 +158,15 @@ var sniff = (function ()
     */
     function sniffy(bytes)
     {
-        var current, input, mimeType, i, pattern,
+        var i, ii, ftType, mime,
             match = false,
+            falltrough = ['text/plain', 'text/plain option2'],
+
+            // Set mimeType to application/octet-stream as a last resort
+            mimeType = 'application/octet-stream',
             notImplemented = [];
+
+        // TODO: Use only the first 1024 bytes or so from `bytes`, to prevent creating huge strings
 
         /**
         * @param {number} nr
@@ -172,35 +178,112 @@ var sniff = (function ()
         }
 
         /**
+        * @param {string | regexp} inp, ptrn
+        * @return {boolean}
+        */
+        function testRegexp(inp, ptrn)
+        {
+            return ptrn.test(inp);
+        }
+
+        /**
         * @param {Uint8Array | Array<number>} inp, ptrn
         * @return {boolean}
         */
-        function isMime(inp, ptrn)
+        function testPattern(values)
         {
-            var j;
+            var j, jj, pattern,
+                input = values[0],
+                patterns = values[1];
 
-            if (inp.length === ptrn.length)
+            for (j = 0; j < patterns.length; j++)
             {
-                for (j = 0; j < ptrn.length; j++)
+                pattern = patterns[j];
+
+                // Check for regexp or non-matching patternBytes/inputBytes
+                if (pattern instanceof RegExp)
                 {
-                    // If true check if first value of inp and ptrn match
-                    if (inp[j] !== ptrn[j])
+                    return testRegexp(input, pattern);
+                }
+                else if (input.length !== pattern.length)
+                {
+                    return false;
+                }
+
+                // Check each inputByte against patternByte
+                for (jj = 0; jj < pattern.length; jj++)
+                {
+                    if (input[jj] !== pattern[jj])
                     {
                         return false;
                     }
-                    else if (inp[j] === ptrn[j] && j === ptrn.length - 1)
+                    else if (input[jj] === pattern[jj] && jj === pattern.length - 1)
                     {
                         return true;
                     }
                 }
             }
-            else if (ptrn instanceof RegExp)
+            return false;
+        }
+
+        /**
+        * @param {string} testType
+        * @return {Array} [input,pattern]
+        */
+        function setTestValues(testType)
+        {
+            var input, patterns,
+                inputString = String.fromCharCode.apply(null, bytes);
+
+            // If regexp pattern exists, test against input string
+            if (testType.regexp)
             {
-                // TODO: implement matching with regexp patterns
-                console.log('Regexp matching is currently not implemented!');
+                patterns = testType.regexp;
+                input = inputString;
             }
 
-            return false;
+            // If no regexp pattern, check if a single pattern exists
+            else if (testType.patterns)
+            {
+                patterns = testType.patterns;
+
+                // Create array around single pattern values
+                if (typeof patterns[0] == 'number')
+                {
+                    patterns = [patterns];
+                }
+
+                // Check if bytenumber mapping exists
+                if (testType.byteNr)
+                {
+                    input = testType.byteNr.map(inputByteAt);
+                }
+                else
+                {
+                    // log mimeType if no bytenumber mapping exists
+                    notImplemented.push(mimeType);
+                    throw new Error('No byte map provided for ' + mimeType);
+                }
+            }
+            else
+            {
+                notImplemented.push(mimeType);
+            }
+
+            return [input, patterns];
+        }
+
+        /**
+        * @param {String} mime
+        * @return {Boolean}
+        */
+        function isMime(thisMime)
+        {
+            // Set testvalues based on current mimeType
+            var testValues = setTestValues(thisMime);
+
+            // Test if current mimeType matches
+            return testPattern(testValues);
         }
 
         // For each mime type
@@ -208,44 +291,33 @@ var sniff = (function ()
         {
             if (bytePatterns.hasOwnProperty(mimeType))
             {
-                current = bytePatterns[mimeType];
+                mime = bytePatterns[mimeType];
 
-                // Create array of relevant bytes for mimesniff input
-                if (current.byteNr)
+                // Exclude falltrough mimeTypes from this first set of tests
+                if (falltrough.indexOf(mimeType) === -1)
                 {
-                    input = current.byteNr.map(inputByteAt);
-                }
-                else
-                {
-                    notImplemented.push(mimeType);
-                    input = [];
-                }
-
-                if (current.patterns && typeof current.patterns[0] == 'number')
-                {
-                    pattern = current.patterns;
-                }
-                else if (current.patterns && current.patterns.length > 1)
-                {
-                    // TODO: implement for multiple patterns
-                    pattern = current.patterns[0];
-                }
-                else
-                {
-                    notImplemented.push(mimeType);
+                    match = isMime(mime);
+                    if (match)
+                    {
+                        mimeType = mimeType;
+                        break;
+                    }
                 }
 
-                if (isMime(input, pattern))
+                // Test if one of the falltrough mimeTypes matches
+                if (!match)
                 {
-                    match = true;
-                    break;
+                    for (i = 0; i < falltrough.length; i++)
+                    {
+                        ftType = bytePatterns[falltrough[i]];
+                        match = isMime(ftType);
+                        if (match)
+                        {
+                            mimeType = ftType;
+                        }
+                    }
                 }
             }
-        }
-
-        if (!match)
-        {
-            mimeType = 'application/octet-stream';
         }
 
         //  console.log('TODO: implement: ' + notImplemented.join(', '));
